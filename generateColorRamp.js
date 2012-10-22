686a793bed299ea39be4441af2eac57582c76d3f
martin <martin.dueren@gmail.com>
2012-10-22 11:08:12 +0200

    initial commit

---
+++ generateColorRamp.js
@@ -0,0 +1,33 @@
+
+
+function interpolate(pBegin, pEnd, pStep, pMax) {
+  if (pBegin < pEnd) {
+    return ((pEnd - pBegin) * (pStep / pMax)) + pBegin;
+  } else {
+    return ((pBegin - pEnd) * (1 - (pStep / pMax))) + pEnd;
+  }
+
+}
+
+function generateColor(theColorBegin, theColorEnd, theNumSteps){
+	var colorArray = new Array();
+	theColorBegin = parseInt(theColorBegin, 16);
+	theColorEnd = parseInt(theColorEnd, 16);
+
+	theR0 = (theColorBegin & 0xff0000) >> 16;
+	theG0 = (theColorBegin & 0x00ff00) >> 8;
+	theB0 = (theColorBegin & 0x0000ff) >> 0;
+	theR1 = (theColorEnd & 0xff0000) >> 16;
+	theG1 = (theColorEnd & 0x00ff00) >> 8;
+	theB1 = (theColorEnd & 0x0000ff) >> 0;
+
+  for (i = 0; i <= theNumSteps; i++) {
+    theR = interpolate(theR0, theR1, i, theNumSteps);
+    theG = interpolate(theG0, theG1, i, theNumSteps);
+    theB = interpolate(theB0, theB1, i, theNumSteps);
+    theVal = ((( theR << 8 ) |  theG ) << 8 ) | theB;
+    colorArray[i] = theVal.toString(16);
+    console.log(theVal.toString(16));
+    }
+    return colorArray;
+}
---
+++ wipeMenu.js
@@ -0,0 +1,102 @@
+dojo.require("dojo.fx");
+var breite = window.innerWidth-450;
+//var breite = document.getElementById("map").width;
+//console.log("breite: " + breite);
+var classesBtn = null;
+var legendBtn = null;
+var classesBtnClicked = false;
+var legendBtnClicked = false;
+
+
+
+    dojo.ready(function(){
+        var slideAwayButton_layer = dojo.byId("slideAwayButton_layer"),
+            slideBackButton_layer = dojo.byId("slideBackButton_layer"),
+            slideTarget_layer = dojo.byId("slideTarget_layer"),
+			
+			slideAwayButton_classes = dojo.byId("slideAwayButton_classes"),
+            slideBackButton_classes = dojo.byId("slideBackButton_classes"),
+            slideTarget_classes = dojo.byId("slideTarget_classes"),
+		    
+			slideAwayButton_legend = dojo.byId("slideAwayButton_legend"),
+            slideBackButton_legend = dojo.byId("slideBackButton_legend"),
+            slideTarget_legend = dojo.byId("slideTarget_legend"),
+            slideAwayButton_split = dojo.byId("slideAwayButton_split");
+		
+		
+		var layerBtnClicked = false;
+		dojo.connect(slideAwayButton_layer, "onclick", function(evt){
+		//console.log("map width: " + map.width);
+            if(!layerBtnClicked){
+			dojo.fx.combine([
+                dojo.fadeIn({ node: slideTarget_layer }),
+                dojo.fx.slideTo({ node: slideTarget_layer, left: map.width-450, top: "0" })
+            ]).play();
+			layerBtnClicked = true;
+			}else{
+				dojo.fx.combine([
+                dojo.fx.slideTo({ node: slideTarget_layer, left: map.width+50, top: "0" }),
+                dojo.fadeOut({ node: slideTarget_layer })
+				]).play();
+				layerBtnClicked = false;
+			}
+        });
+        dojo.connect(slideBackButton_layer, "onclick", function(evt){
+            dojo.fx.combine([
+                dojo.fx.slideTo({ node: slideTarget_layer, left: map.width+50, top: "0" }),
+                dojo.fadeOut({ node: slideTarget_layer })
+            ]).play();
+			layerBtnClicked = false;
+        });
+		
+		
+		enableClasses();
+		
+        dojo.connect(slideBackButton_classes, "onclick", function(evt){
+            dojo.fx.combine([
+                dojo.fx.slideTo({ node: slideTarget_classes, left: map.width+50, top: "220" }),
+                dojo.fadeOut({ node: slideTarget_classes })
+            ]).play();
+			classesBtnClicked = false;
+        });
+		
+		enableLegend();
+		
+		
+        dojo.connect(slideBackButton_legend, "onclick", function(evt){
+            dojo.fx.combine([
+                dojo.fx.slideTo({ node: slideTarget_legend, left: "-500", top: "300" }),
+                dojo.fadeOut({ node: slideTarget_legend })
+            ]).play();
+			legendBtnClicked = false;
+        });
+		var dualView = false;
+        dojo.connect(slideAwayButton_split, "onclick", function(evt){
+			if(dualView){
+				var fs = parent.document.getElementById("frameset");
+				fs.removeChild(parent.document.getElementById("frame2"));
+				var fs = parent.document.getElementById("frameset");
+				fs.cols = "100%";
+				dualView = false;
+			}else{
+				var fs = parent.document.getElementById("frameset"),
+				f2 = top.document.createElement('frame');
+				fs.cols = "50%,50%";
+				f2.name = "frame2";
+				f2.id = "frame2";
+				f2.src = "map.html";
+				fs.appendChild(f2);
+				dualView = true;
+				}
+        });
+		
+    });
+
+	
+
+	
+	
+
+	
+	
+	
